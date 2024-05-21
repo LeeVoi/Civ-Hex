e08b6fcfde4ba98a891c10575270b0551553f628
LeeVoi <Abual3a7ed.6@gmail.com>
2024-05-21 13:03:26 +0200

    small changes to the home screen

--- frontend/civ_hex/lib/components/home_screen.dart
+++ frontend/civ_hex/lib/components/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'game_screen.dart'; // Import the GameScreen file
+import 'game_screen.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -12,14 +14,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue, // Set background color to blue
+      backgroundColor: Colors.blue,
       body: Stack(
         children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              SizedBox(height: 50),
-              Expanded(
+              const SizedBox(height: 50),
+              const Expanded(
                 child: Center(
                   child: Text(
                     'Home Screen',
@@ -27,11 +29,11 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Expanded(
                 flex: 2,
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
@@ -44,7 +46,6 @@ class _HomeScreenState extends State<HomeScreen> {
                               MaterialPageRoute(builder: (context) => GameScreen()),
                             );
                           },
-                          child: Text('Play'),
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                             foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
@@ -54,9 +55,10 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ),
                           ),
+                          child: const Text('Play'),
                         ),
                       ),
-                      SizedBox(width: 20),
+                      const SizedBox(width: 20),
                       Expanded(
                         child: ElevatedButton(
                           onPressed: () {
@@ -64,7 +66,6 @@ class _HomeScreenState extends State<HomeScreen> {
                               _showRulesPopup = true; // Show the rules popup
                             });
                           },
-                          child: Text('Rules'),
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                             foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
@@ -74,6 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ),
                           ),
+                          child: const Text('Rules'),
                         ),
                       ),
                     ],
@@ -89,7 +91,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Container(
                   width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
                   height: MediaQuery.of(context).size.height * 0.5, // 50% of screen height
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(20),
@@ -97,32 +99,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
+                      const Text(
                         'Game Rules',
                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'The game Rules for Civ-Hex are fairly simple. '+
-                        'When u join the game u are asked to select three starting tiles, '+
-                        'You can only own tiles that are touching eachother. '+
-                        'The goal of the game is to reach 50 Victory Points, '+
-                        'You earn 1 victory point for each tile you own, once you reach 5 tiles u get a bonus 5 VP, '+
-                        'the same applies for population. ' +
-                        'Tile price will increase based on how many tiles you own, '+
-                        'Population will cost you 1 of each resource type. '+
-                        'you role a dice and you earn the resources for the tiles that have the number the dice roled on. '+
-                        'Also you can earn gold buy selling resources, one of any resource type will trade for one gold.',
+                      const SizedBox(height: 20),
+                      const Text(
+                        'The game Rules for Civ-Hex are fairly simple. When u join the game u are asked to select three starting tiles, You can only own tiles that are touching each-other. The goal of the game is to reach 50 Victory Points, You earn 1 victory point for each tile you own, once you reach 5 tiles u get a bonus 5 VP, the same applies for population. Tile price will increase based on how many tiles you own, Population will cost you 1 of each resource type. you role a dice and you earn the resources for the tiles that have the number the dice rolled on. Also you can earn gold buy selling resources, one of any resource type will trade for one gold.',
                         style: TextStyle(fontSize: 16),
                       ),
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       ElevatedButton(
                         onPressed: () {
                           setState(() {
                             _showRulesPopup = false; // Hide the rules popup
                           });
                         },
-                        child: Text('Close'),
+                        child: const Text('Close'),
                       ),
                     ],
                   ),
